b4e73525d98eb04b4e54ed34e6e0a39a6b2b936f
Michel Rego <43715929+michelrego@users.noreply.github.com>
2023-06-16 22:44:15 +0530

    Add files via upload

---
+++ src/test/java/com/lilspicylilsweet/landl/LandlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilspicylilsweet.landl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
